d1d812199c98f6ad2a74b57c18f1929c43813096
selvamurugan <144770140+selvamurugan22@users.noreply.github.com>
2024-03-19 12:03:44 +0530

    Add files via upload

---
+++ front-end/Day-1/src/pages/admin/Dashboardcontent.jsx
@@ -0,0 +1,20 @@
+import Dashboard from "../../components/Dashboard"
+
+const Dashboardcontent = () => {
+  return (
+    <>
+      <Dashboard />
+      <div className="container">
+        <div className="content">
+          <h2>Welcome to the Admin Dashboard!</h2>
+          <p>
+            This is a simple example of an admin dashboard using HTML and CSS.
+          </p>
+          <p>You can add more content and functionality as needed.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Dashboardcontent
