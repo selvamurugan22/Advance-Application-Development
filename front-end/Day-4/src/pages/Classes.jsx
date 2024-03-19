3b8250185e205c9cb0330fffe1bb845a1225e67a
selvamurugan <144770140+selvamurugan22@users.noreply.github.com>
2024-03-19 12:11:30 +0530

    Add files via upload

---
+++ front-end/Day-4/src/pages/Classes.jsx
@@ -0,0 +1,19 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+
+const Classes = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="container mt-5">
+        some classes contents
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default Classes
