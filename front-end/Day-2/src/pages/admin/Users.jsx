c993bed080f976fdf02550115ec509b58fdd91db
selvamurugan <144770140+selvamurugan22@users.noreply.github.com>
2024-03-19 12:07:39 +0530

    Add files via upload

---
+++ front-end/Day-2/src/pages/admin/Users.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+import Dashboard from '../../components/Dashboard'
+
+const Users = () => {
+  return (
+    <>
+    <Dashboard/>
+    <div className='container'>
+        <div className='content'>
+            users
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Users
