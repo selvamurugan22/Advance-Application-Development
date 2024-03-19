f7c0dea973efcd8b7b997777e2dae0c66f631400
selvamurugan <144770140+selvamurugan22@users.noreply.github.com>
2024-03-19 12:09:33 +0530

    Add files via upload

---
+++ front-end/Day-3/src/pages/admin/AdminClass.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+import Dashboard from "../../components/Dashboard"
+
+const AdminClass = () => {
+  return (
+    <>
+    <Dashboard/>
+    <div className="container">
+        <div className="content">
+            adminclass
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default AdminClass
